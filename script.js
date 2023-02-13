561e7e7e197973dcae9986a2c608a4d0ee795add
Josep Abellana <josep.abe.pu@gmail.com>
2023-02-13 22:59:58 +0100

    x

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ var chart = document.getElementById('chart');
 (async ()=>{
     const data = await fetch('./data.json').then(res => res.json());
     for(let i = 0;i<data.length;i++){
+        let text = document.createElement('p');
+        let div = document.createElement('div');
+        div.classList.add('column')
+        div.style.height = `${data[i]['amount']}px`;
         
+        text.textContent = `${data[i].day}`;
+        chart.append(div)
+        chart.append(text)
     }
 })()
