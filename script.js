3107984bf768da031bc8527ea6dd7c922bf23f16
Josep Abellana <josep.abe.pu@gmail.com>
2023-02-13 23:08:02 +0100

    x

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ var chart = document.getElementById('chart');
 (async ()=>{
     const data = await fetch('./data.json').then(res => res.json());
     for(let i = 0;i<data.length;i++){
-        let text = document.createElement('p');
         let div = document.createElement('div');
         div.classList.add('column')
-        div.style.height = `${data[i]['amount']}px`;
-        
+        div.style.height = `${data[i]['amount']*2}px`;
+        chart.append(div);
+    }
+    for(let i = 0;i<data.length;i++){
+        let text = document.createElement('p');
         text.textContent = `${data[i].day}`;
-        chart.append(div)
-        chart.append(text)
+        chart.append(text);
     }
 })()
